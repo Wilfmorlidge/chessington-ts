9c7b30131fbaab128efaff74f676839f3135210c
Wilfmorlidge <wilfmorlidge@gmail.com>
2024-06-26 17:28:51 +0100

    fixed knight movement

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -13,30 +13,34 @@ export default class Knight extends Piece {
     public getAvailableMoves(board: Board) {
         var output: Square[] = [];
         var pos = board.findPiece(this);
-        if (pos.row+2 <= GameSettings.BOARD_SIZE-1 && pos.col+1 <= GameSettings.BOARD_SIZE-1) {
-            output.push(Square.at((pos.row+2),(pos.col+1)));
-        }
-        if (pos.row+2 <= GameSettings.BOARD_SIZE-1 && pos.col-1 >= 0) {
-            output.push(Square.at((pos.row+2),(pos.col-1)));
-        }
-        if (pos.row+1 <= GameSettings.BOARD_SIZE-1 && pos.col+2 <= GameSettings.BOARD_SIZE-1) {
-            output.push(Square.at((pos.row+1),(pos.col+2)));
-        }
-        if (pos.row-1 >= 0 && pos.col+2 <= GameSettings.BOARD_SIZE-1) {
-            output.push(Square.at((pos.row-1),(pos.col+2)));
-        }
         if (pos.row-2 >= 0 && pos.col+1 <= GameSettings.BOARD_SIZE-1) {
             output.push(Square.at((pos.row-2),(pos.col+1)));
         }
         if (pos.row-2 >= 0 && pos.col-1 >= 0) {
             output.push(Square.at((pos.row-2),(pos.col-1)));
         }
-        if (pos.row+1 <= GameSettings.BOARD_SIZE-1 && pos.col-2 >= 0) {
-            output.push(Square.at((pos.row+1),(pos.col-2)));
-        }
         if (pos.row-1 >= 0 && pos.col+2 <= GameSettings.BOARD_SIZE-1) {
             output.push(Square.at((pos.row-1),(pos.col+2)));
         }
+        if (pos.row-1 <= GameSettings.BOARD_SIZE-1 && pos.col-2 >= 0) {
+            output.push(Square.at((pos.row-1),(pos.col-2)));
+        }
+
+        if (pos.row+2 <= GameSettings.BOARD_SIZE-1 && pos.col+1 <= GameSettings.BOARD_SIZE-1) {
+            output.push(Square.at((pos.row+2),(pos.col+1)));
+        }
+
+        if (pos.row+2 <= GameSettings.BOARD_SIZE-1 && pos.col-1 >= 0) {
+            output.push(Square.at((pos.row+2),(pos.col-1)));
+        }
+        if (pos.row+1 <= GameSettings.BOARD_SIZE-1 && pos.col+2 <= GameSettings.BOARD_SIZE-1) {
+           output.push(Square.at((pos.row+1),(pos.col+2)));
+        }
+
+        if (pos.row+1 <= GameSettings.BOARD_SIZE-1 && pos.col-2 >= 0) {
+            output.push(Square.at((pos.row+1),(pos.col-2)));
+        }
+  
 
         return output
     }
