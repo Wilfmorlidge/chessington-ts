303ca42d738a95791a96087449847ee7097c6499
Wilfmorlidge <wilfmorlidge@gmail.com>
2024-06-26 15:15:17 +0100

    changed mvement of pawns and rooks

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -4,18 +4,38 @@ import Board from '../board';
 import Square from '../square';
 
 export default class Pawn extends Piece {
+    public has_moved: boolean = false;
+    public player1: Player = Player.WHITE;
     public constructor(player: Player) {
         super(player);
+        this.player1 = player
     }
 
     public getAvailableMoves(board: Board) {
         var output: Square[] = [];
         var pos = board.findPiece(this);
-        output.push(Square.at((pos.row+1),(pos.col)));
-        output.push(Square.at((pos.row-1),(pos.col)));
+        if (this.player1 == Player.WHITE) {
+            output.push(Square.at((pos.row+1),(pos.col)));
+            if (this.has_moved == false) {
+                output.push(Square.at((pos.row+2),(pos.col)));
+            }
+        }
+        if (this.player1 == Player.BLACK) {
+            output.push(Square.at((pos.row-1),(pos.col)));
+            if (this.has_moved == false) {
+                output.push(Square.at((pos.row-2),(pos.col)));
+            }
+        }
         return output;
         //identify pawns position
         // identify which adjacent positions are occupied
         // for every unoccupied position, append to array
     }
+
+    public moveTo(board: Board, newSquare: Square) {
+        const currentSquare = board.findPiece(this);
+        board.movePiece(currentSquare, newSquare);
+        this.has_moved = true
+    }
+
 }
--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,13 +1,32 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Rook extends Piece {
+    public player1: Player = Player.WHITE;
     public constructor(player: Player) {
         super(player);
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        var output: Square[] = [];
+        var pos = board.findPiece(this);
+        for (let i = 1;i <= (GameSettings.BOARD_SIZE-1);i++) {
+            if (pos.col+i <= GameSettings.BOARD_SIZE-1) {
+                output.push(Square.at((pos.row),(pos.col+i)));
+            }
+            if (pos.col-i >= 0) {
+                output.push(Square.at((pos.row),(pos.col-i)));
+            }
+            if (pos.row+i <= GameSettings.BOARD_SIZE-1) {
+                output.push(Square.at((pos.row+i),(pos.col)));
+            }
+            if (pos.row-i >= 0) {
+                output.push(Square.at((pos.row-i),(pos.col)));
+            }
+        }
+        return output;
     }
 }
