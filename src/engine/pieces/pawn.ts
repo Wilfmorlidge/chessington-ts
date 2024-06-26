6ac0ac0ae86b6d1329f0274bdf25828f84edfa36
Wilfmorlidge <wilfmorlidge@gmail.com>
2024-06-26 14:33:59 +0100

    allowed pawns to move one space

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,13 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        var output: Square[] = [];
+        var pos = board.findPiece(this);
+        output.push(Square.at((pos.row+1),(pos.col)));
+        output.push(Square.at((pos.row-1),(pos.col)));
+        return output;
+        //identify pawns position
+        // identify which adjacent positions are occupied
+        // for every unoccupied position, append to array
     }
 }
